f261e4e53ae4886892f2b8b7607457bacef11389
miguelAngel14 <61488303+miguelAngel14@users.noreply.github.com>
2020-06-04 22:15:38 -0500

    Create clasepia.py

---
+++ clasepia.py
@@ -0,0 +1,12 @@
+class CONTACTO:
+    def __init__(self,NickName,Nombre,Correo,Telefono,FechaNacimiento,Gasto):
+        self.NickName = NickName
+        self.Nombre= Nombre
+        self.Correo = Correo
+        self.Telefono= Telefono
+        self.FechaNacimiento = str(FechaNacimiento)
+        self.Gasto= Gasto
+   #Se definio la clase del objeto de contacto
+   #se establecio un metodo constructor para cada atributo del objeto contacto
+   #Esblecimos un metodo para instanciar cada atributo de objeto
+
